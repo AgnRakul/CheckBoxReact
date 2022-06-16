7e69300e60bb35e7db72a74cd5f2e6718a23543b
RakulAgn <rakul0agn@gmail.com>
2022-06-16 21:34:03 +0530

    Push

--- src/Invite.jsx
+++ src/Invite.jsx
@@ -1,47 +1,76 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { RadiobuttonContent } from "./Data";
 import { SensitivePermissionAutomation } from "./Data";
 
 const Invite = () => {
+  const [checkAutomation, setcheckAutomation] = useState(false);
+  const [checkSubVal, setcheckSubVal] = useState(false);
+  const [disableSubCheckBox, setdisableSubCheckBox] = useState(false);
+  const [checkRadio, setCheckRadio] = useState(false);
+  const [isSelected, setSelected] = useState();
+
+  // const ref = useRef(checkSubVal);
+
+  const FindRadioVal = (e) => {
+    setSelected(e);
+    if (e.value === "Admin") {
+      setcheckAutomation(true);
+      setcheckSubVal(true);
+      setdisableSubCheckBox(true);
+    }
+  };
+  const reverseCheck = (e) => {
+    console.log(e.value);
+    console.log(isSelected);
+    setCheckRadio(!checkRadio);
+    setcheckSubVal(true)
+    setcheckAutomation(!checkAutomation);
+    setdisableSubCheckBox(!disableSubCheckBox);
+  };
+  const subCheckButton = () => {
+    setcheckSubVal(!checkSubVal)
+  }
+
   return (
-    <form action="#" method="POST">
-      <div className="px-4 py-5 bg-white space-y-6 sm:p-6 shadow sm:rounded-md ">
-        <div className="mt-10 sm:mt-0">
-          <div className="md:grid md:grid-cols-3 md:gap-6">
-            <div className="mt-5 md:mt-0 md:col-span-2">
-              <div className="shadow overflow-hidden sm:rounded-md">
-                <div className="px-4 py-5 bg-white space-y-6 sm:p-6">
-                  <fieldset>
-                    <div className="flex items-center gap-4  ">
-                      {RadiobuttonContent.map((radioValue, i) => {
-                        return (
-                          <div
-                            key={i}
-                            className={`flex items-start border  rounded p-3 hover:cursor-pointer  `}
-                          >
-                            <div className="flex items-center h-5 ">
-                              <input
-                                id="flexRadioDefault1"
-                                name="flexRadioDefault1"
-                                value={radioValue}
-                                type="radio"
-                                className={`focus:ring-white  h-4 w-4 text-green-500 border-gray-300 rounded-full hover:cursor-pointer`}
-                              />
-                              <label
-                                className="ml-3 text-sm font-medium text-gray-500 hover:cursor-pointer"
-                                htmlFor="Admin"
-                              >
-                                {radioValue}
-                              </label>
-                            </div>
+    <div className="px-4 py-5 bg-white space-y-6 sm:p-6 shadow sm:rounded-md ">
+      <div className="mt-10 sm:mt-0">
+        <div className="md:grid md:grid-cols-3 md:gap-6">
+          <div className="mt-5 md:mt-0 md:col-span-2">
+            <div className="shadow overflow-hidden sm:rounded-md">
+              <div className="px-4 py-5 bg-white space-y-6 sm:p-6">
+              
+                  <div className="flex items-center gap-4  ">
+                    {RadiobuttonContent.map((radioValue, i) => {
+                      return (
+                        <div
+                          key={i}
+                          className={`flex items-start border  rounded p-3 hover:cursor-pointer  `}
+                        >
+                          <div className="flex items-center h-5 ">
+                            <input
+                              id="flexRadioDefault1"
+                              name="flexRadioDefault1"
+                              onClick={(e) => FindRadioVal(e.target)}
+                              value={radioValue}
+                              type="radio"
+                              className={`focus:ring-white  h-4 w-4 text-green-500 border-gray-300 rounded-full hover:cursor-pointer`}
+                            />
+                            <label
+                              className="ml-3 text-sm font-medium text-gray-500 hover:cursor-pointer"
+                              htmlFor="Admin"
+                            >
+                              {radioValue}
+                            </label>
                           </div>
-                        );
-                      })}
-                    </div>
-                    <div className="flex flex-col my-4">
-                      {SensitivePermissionAutomation.map((ReferralData) => {
+                        </div>
+                      );
+                    })}
+                  </div>
+                  <div className="flex flex-col my-4">
+                    {SensitivePermissionAutomation.map(
+                      (ReferralData, index) => {
                         return (
-                          <div className="flex flex-col my-4">
+                          <div key={index} className="flex flex-col my-4">
                             <strong className="uppercase text-gray-600 font-medium">
                               {ReferralData.Category}
                             </strong>
@@ -50,6 +79,11 @@ const Invite = () => {
                                 <input
                                   id="referral"
                                   name="comments"
+                                  checked={checkAutomation}
+                                  value={
+                                    ReferralData.HeadingCheck.permissionsName
+                                  }
+                                  onClick={(e) => reverseCheck(e.target)}
                                   type="checkbox"
                                   className="focus:ring-white h-4 w-4 text-green-500 border-gray-300 rounded"
                                 />
@@ -64,15 +98,29 @@ const Invite = () => {
                                 <p> {ReferralData.HeadingCheck.description}</p>
                               </div>
                             </div>
-                            {ReferralData.Permissions.map((referral) => {
+                            {ReferralData.Permissions.map((referral, i) => {
                               return (
-                                <div className="ml-5 pb-2 flex items-start mt-1">
-                                  <input type="checkbox" name="" id="" />
-                                  <p
-                                    className="w-1/2 ml-auto 
-                      text-[#5A5B72]
-                    "
-                                  >
+                                <div
+                                  key={i}
+                                  className="ml-5 pb-2 flex items-start mt-1"
+                                >
+                                  <input
+                                    id={referral.permissionsName}
+                                    value={referral.permissionsName}
+                                    name={referral.permissionsName}
+                                    checked={checkSubVal}
+                                    onClick={() => subCheckButton()}
+                                    disabled={disableSubCheckBox}
+                                    type="checkbox"
+                                    className="focus:ring-white h-4 w-4 text-green-500 border-gray-300 rounded"
+                                  />
+                                  <label
+                                  className="ml-3 text-sm font-medium text-gray-500 hover:cursor-pointer"
+                                  htmlFor="referral"
+                                >
+                                  {referral.permissionsName}
+                                </label>
+                                  <p className="w-1/2 ml-auto text-[#5A5B72]">
                                     {referral.description}
                                   </p>
                                 </div>
@@ -80,22 +128,16 @@ const Invite = () => {
                             })}
                           </div>
                         );
-                      })}
-                    </div>
-                  </fieldset>
-                </div>
+                      }
+                    )}
+                  </div>
+               
               </div>
             </div>
           </div>
         </div>
-
-        <div className="hidden sm:block" aria-hidden="true">
-          <div className="py-5">
-            <div className="border-t border-gray-200"></div>
-          </div>
-        </div>
       </div>
-    </form>
+    </div>
   );
 };
 
